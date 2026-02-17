f3546380686c91f6fbac4afc4e364bec61946e01
Alvaro Lopez Ciller <alloci@MacBook-Pro-de-Alvaro.local>
2026-02-17 16:40:53 +0100

    Add environment verification script

---
+++ scripts/verify.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+
+echo "Checking Git..."
+git --version
+
+echo "Checking GitHub CLI..."
+gh --version
+
+echo "Checking Terraform..."
+terraform -version
+
+echo "Checking Azure CLI..."
+az version
+
+echo "Checking Ansible..."
+ansible --version
+
+echo "Checking Docker..."
+docker --version
